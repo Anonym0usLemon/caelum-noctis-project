51de12b9ee91070c2a2fd7d518dfb394f52912ff
Dominic DiModugno <anonym0uslemon@Dominics-Laptop.local>
2025-01-04 17:10:53 -0600

    finished gallery file upload form

--- src/Controller/GalleryManagementController.php
+++ src/Controller/GalleryManagementController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use App\Entity\Image;
+use App\Form\ImageType;
+use App\Service\FileUploader;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,14 +15,29 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 class GalleryManagementController extends AbstractController
 {
     #[Route('/gallery/add', name: 'gallery_add')]
-    public function index(
-        Request $request, 
-        SluggerInterface $slugger,
-        #[Autowire('%kernel.project_dir%/public/images')] string $imagesDirectory
-        ): Response
+    public function add(Request $request, FileUploader $fileUploader, SluggerInterface $slugger, EntityManagerInterface $entityManager): Response 
     {
+        $image = new Image();
+        $form = $this->createForm(ImageType::class, $image); 
+        
+        $form->handleRequest($request); 
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $uploadedFile = $form->get('imageFile')->getData(); 
+
+            if ($uploadedFile) {
+                $newFilename = $fileUploader->upload($uploadedFile); 
+                $image->setFilename($newFilename); 
+            }
+
+            $entityManager->persist($image); 
+            $entityManager->flush(); 
+
+            return $this->redirectToRoute('gallery');
+        }
+
         return $this->render('gallery_management/index.html.twig', [
-            'controller_name' => 'GalleryManagementController',
+            'form' => $form->createView(), 
         ]);
     }
 }
---
+++ src/Form/ImageType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Image;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('imageFile', FileType::class, [
+                'label' => 'Image (JPG, JPEG, or PNG file)',
+                'mapped' => false,
+                'required' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Upload',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Image::class,
+        ]);
+    }
+}
